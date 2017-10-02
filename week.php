570e0ab0aa3312e5d81c913520d75cd9a764adac
Waterticket <matthew218@naver.com>
2017-10-02 15:43:25 +0900

    Update week.php

--- week.php
+++ week.php
@@ -3,11 +3,17 @@
 made by Waterticket
 matthew218@naver.com
 */
-$officecode = "";
-$schulCode = "";
-$schulCrseScCode = "3";
-$schulKndScCode = "03";
-$schMmealScCode = "2";
+$schulCode = ""; //학교 코드
+$officecode = ""; //교육청 코드
+$schulScCode = ""; //학교 분류 코드
+$schMmealCode = ""; //급식 종류 코드
 $schYmd = date("Y.m.d", mktime(0,0,0,date("m")  , date("d")+1, date("Y"))); //내일 날짜
 
-$url = 'http://stu.';
+$food_url = 'http://stu.'.$officecode.'/sts_sci_md01_001.do?schulCode='.$schulCode.'&schulCrseScCode='.$schulScCode.'&schMmealScCode='.$schMmealCode.'&schYmd='.$schYmd;
+$text = file_get_contents($food_url); 
+$temp = @explode('<table', $text); 
+$tmp = '<table border="1" '.$temp[1];
+$temp = @explode('</table>', $tmp); 
+
+echo $temp[0];
+?>
