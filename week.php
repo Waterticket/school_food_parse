8a7214708cf67bbea5462f503ca645a70d6e321a
Waterticket <matthew218@naver.com>
2017-10-02 15:11:12 +0900

    Create week.php

---
+++ week.php
@@ -0,0 +1,13 @@
+<?php
+/*
+made by Waterticket
+matthew218@naver.com
+*/
+$officecode = "";
+$schulCode = "";
+$schulCrseScCode = "3";
+$schulKndScCode = "03";
+$schMmealScCode = "2";
+$schYmd = date("Y.m.d", mktime(0,0,0,date("m")  , date("d")+1, date("Y"))); //내일 날짜
+
+$url = 'http://stu.';
